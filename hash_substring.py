1bb356c11fce7a883a61becdaf3a2a41a030ca1c
NilsPolis <130793264+NilsPolis@users.noreply.github.com>
2023-04-14 22:30:28 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,59 +1,56 @@
-def read_file_input(filename):
-    try:
-        with open(f"./tests/{filename}") as f:
-            contents = f.readlines()
-    except FileNotFoundError:
-        raise ValueError("File not found")
-    except:
-        raise ValueError("Error reading file")
-    
-    pattern = contents[0].strip()
-    text = contents[1].strip()
-    
-    return pattern, text
+# python3
+def read_input(filename=None):
+    if filename is not None:
+        try:
+            with open(f"./test/{filename}") as f:
+                contents = f.readlines()
+        except FileNotFoundError:
+            raise ValueError("Missing File")
+        except:
+            raise ValueError("Reading Error")
 
-def read_user_input():
-    pattern = input().rstrip()
-    text = input().rstrip()
-    
-    return pattern, text
-
-def read_input():
-    input_type = input().rstrip()
-    
-    if input_type == 'I':
-        return read_user_input()
-    elif input_type == 'F':
-        filename = "06"
-        if str(filename[-1]) == "a":
-            raise ValueError("Invalid filename")
-        return read_file_input(filename)
+        pattern = contents[0].strip()
+        text = contents[1].strip()  
     else:
-        raise ValueError("Invalid input type")
+        pattern = input().rstrip()
+        text = input().rstrip()
+
+    return pattern, text
+
 def print_occurrences(output):
     print(' '.join(map(str, output)))
-
 
 def get_occurrences(pattern, text):
     prime = 101
     base = 256
 
-    pattern_hash = 0
-    text_hash = 0
+    p_hash = 0
+    t_hash = 0
     for i in range(len(pattern)):
-        pattern_hash = (pattern_hash * base + ord(pattern[i])) % prime
-        text_hash = (text_hash * base + ord(text[i])) % prime
+        p_hash = (p_hash * base + ord(pattern[i])) % prime
+        t_hash = (t_hash * base + ord(text[i])) % prime
 
     occurrences = []
     for i in range(len(text) - len(pattern) + 1):
-        if pattern_hash == text_hash:
+        if p_hash == t_hash:
             if pattern == text[i:i+len(pattern)]:
                 occurrences.append(i)
         if i < len(text) - len(pattern):
-            text_hash = ((text_hash - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
+            t_hash = ((t_hash - ord(text[i]) * (base**(len(pattern)-1))) * base + ord(text[i+len(pattern)])) % prime
 
     return occurrences
 
-
 if __name__ == '__main__':
-    print_occurrences(get_occurrences(*read_input()))
+    input_type = input().rstrip()
+
+    if input_type == 'I':
+        pattern, text = read_input()
+    elif input_type == 'F':
+        filename = "06"
+        if str(filename[-1]) == "a":
+            raise ValueError("Invalid filename")
+        pattern, text = read_input(filename)
+    else:
+        raise ValueError("Invalid input type")
+
+    print_occurrences(get_occurrences(pattern, text))
